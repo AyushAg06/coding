21a972a56aafbdfc9b781d9a26b58c542d003a79
AyushAg06 <ayushagarwala@gmail.com>
2025-01-30 02:18:58 +0530

    commit

--- tuf_practice/array_8.cpp
+++ tuf_practice/array_8.cpp
@@ -1,47 +1,35 @@
 // move zeroes to the end of the array
 #include<bits/stdc++.h>
 using namespace std;
-vector <int> movezeroes(vector<int>arr)
-{
-    int j=INT_MIN;
-    for(int i=0;i<arr.size();i++)
-    {
-        if(arr[i]==0)
-        {
-            j=i;
-            break;
-        }
-    }
-    if(j==INT_MIN)
-    {
-        return arr;
-    }
-    for(int i=j+1;i<arr.size();i++)
-    {
-        if(arr[i]!=0)
-        {
-            swap(arr[i],arr[j]);
+void moveZeroes(vector<int>& nums) {
+    int n = nums.size();
+    int j = 0; // Pointer to keep track of the next position for non-zero elements
+    // Step 1: Move all non-zero elements to the front
+    for (int i = 0; i < n; i++) {
+        if (nums[i] != 0) {
+            nums[j] = nums[i];
             j++;
         }
+    } 
+    // Step 2: Fill the remaining positions with zeros
+    for (int i = j; i < n; i++) {
+        nums[i] = 0;
     }
-    return arr;
 }
-int main()
-{
+int main() {
     int n;
-    cout<<"Enter the size of the array: "<<endl;
-    cin>>n;
-    vector<int>arr(n);
-    cout<<"Enter the elements of the array: "<<endl;
-    for(int i=0;i<n;i++)
-    {
-        cin>>arr[i];
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    vector<int> nums(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
     }
-    vector<int>ans=movezeroes(arr);
-    cout<<"The array after moving zeroes to the end is: "<<endl;
-    for(auto it:ans)
-    {
-        cout<<it<<" ";
+    moveZeroes(nums);
+    cout << "Array after moving zeroes to the end: ";
+    for (int i = 0; i < n; i++) {
+        cout << nums[i] << " ";
     }
+    cout << endl;
     return 0;
 }
