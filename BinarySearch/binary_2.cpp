b53ea08a2fc20faacefa9b1b10a16ea06de8bdd9
AyushAg06 <ayushagarwala@gmail.com>
2025-02-05 21:18:13 +0530

    commit it

---
+++ BinarySearch/binary_2.cpp
@@ -0,0 +1,30 @@
+// binary search using recurrsion
+#include<bits/stdc++.h>
+using namespace std;
+int BinarySearch(vector<int>&nums,int target,int low,int high)
+{
+    if(low>high)
+    {
+        return -1;
+    }
+    int mid=(low+high)/2;
+    if(nums[mid]==target)
+    {
+        return mid;
+    }
+    else if(nums[mid]<target)
+    {
+        return BinarySearch(nums,target,mid+1,high);
+    }
+    else
+    {
+        return BinarySearch(nums,target,low,mid-1);
+    }
+}
+int main()
+{
+    vector<int>nums={1,2,3,4,5,6,7,8,9,10};
+    int target=5;
+    cout<<BinarySearch(nums,target,0,nums.size()-1)<<endl;
+    return 0;
+}
